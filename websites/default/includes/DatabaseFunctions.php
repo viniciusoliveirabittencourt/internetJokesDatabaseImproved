6ea240af80571966ef45cb72352f40d4b34bea36
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2025-09-12 13:02:26 -0300

    Create all generic functions

--- websites/default/includes/DatabaseFunctions.php
+++ websites/default/includes/DatabaseFunctions.php
@@ -62,13 +62,6 @@ function deleteJoke($pdo, $id) {
     $stmt->execute([':id' => $id]);
 }
 
-function findAll($pdo, $table) {
-    $stmt = $pdo->prepare('SELECT * FROM `' . $table . '`');
-    $stmt->execute();
-
-    return $stmt->fetchAll();
-}
-
 function allJokes($pdo) {
     $stmt = $pdo->prepare('SELECT `joke`.`id`, `joketext`, `jokedate`, `name`, `email` FROM `joke` INNER JOIN `author` ON `authorid` = `author`.`id`;');
     $stmt->execute();
@@ -107,4 +100,73 @@ function insertAuthor($pdo, $values) {
 
     $stmt = $pdo->prepare($query);
     $stmt->execute($values);
+}
+
+function delete($pdo, $table, $field, $value) {
+    $values = [':value' => $value];
+    $stmt = $pdo->prepare('DELETE FROM `' . $table . '` WHERE `'. $field .'` = :value');
+    $stmt->execute($values);
+}
+
+function findAll($pdo, $table) {
+    $stmt = $pdo->prepare('SELECT * FROM `' . $table . '`');
+    $stmt->execute();
+
+    return $stmt->fetchAll();
+}
+
+function insert($pdo, $values, $table) {
+    $query = 'INSERT INTO `' . $table . '` (';
+
+    foreach ($values as $key => $value) {
+        $query .= '`' . $key . '`,';
+    }
+    $query = rtrim($query, ',');
+
+
+    $query .= ') VALUES (';
+
+    foreach ($values as $key => $value) {
+        $query .= ':' . $key . ',';
+    }
+    $query = rtrim($query, ',');
+
+    $query .= ')';
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute($values);
+}
+
+function update($pdo, $table, $primarykey, $values) {
+    $query = 'UPDATE ' . $table . ' SET ';
+    $updateFields = [];
+
+    foreach ($values as $key => $value) {
+        if ($value instanceof DateTime) {
+            $values[$key] = $value->format('Y-m-d');
+        }
+        $updateFields[] = '`' . $key . '` = :' . $key;
+    }
+    $query .= implode(', ', $updateFields);
+    $query .= ' WHERE `' . $primarykey . '` = :primarykey';
+    $values['primarykey'] = $values['id'];
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute($values);
+}
+
+function find($pdo, $table, $field, $value) {
+    $query = 'SELECT * FROM `' . $table . '` WHERE `'. $field . '` = :value';
+    $values = ['value' => $value];
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute($values);
+    return $stmt->fetchAll();
+}
+
+function total($pdo, $table) {
+    $stmt = $pdo->prepate('SELECT COUNT(*) FROM `' . $table . '`');
+    $stmt->execute();
+    $row = $stmt->fetch();
+    return $row[0];
 }
